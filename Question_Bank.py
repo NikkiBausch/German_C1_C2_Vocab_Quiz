38fc2d03509f516319ccb917849cd86a73bcad65
NikkiBausch <41345938+NikkiBausch@users.noreply.github.com>
2022-01-30 15:57:56 -0600

    Add files via upload

---
+++ Question_Bank.py
@@ -0,0 +1,502 @@
+Quiz_Bank = {
+    1 : { 
+        "question" : "'Erbarmungslos' bedeutet: " , 
+        "answer" : "unrelenting"
+
+    },
+    2 : {
+        "question" : "'durchgreifen' bedeutet: " , 
+        "answer" : "to crackdown"
+    }, 
+
+    3 : {
+        "question" : "'Jaehzornig' bedeuet: " , 
+        "answer" : "short-fused"
+    }, 
+
+    4 : {
+        "question" : "'Launisch' bedeutet: " , 
+        "answer" : "moody"
+    }, 
+
+    5 : {
+        "question" : "'Mitteilsam' bedeutet: " , 
+        "answer" : "communicative"
+    }, 
+
+    6 : {
+        "question" : "'Oberflaechlich' bedeutet: " , 
+        "answer" : "superficial"
+    }, 
+
+    7 : {
+        "question" : "'Rechthaberisch' bedeutet: " , 
+        "answer" : "bossy"
+    }, 
+
+    8 : {
+        "question" : "'Starrsinnig' bedeutet: " , 
+        "answer" : "stubborn"
+    }, 
+
+    9 : {
+        "question" : "'Wenn Jemanden auf andere herabsieht', bedeutet es, dass sie . . ." , 
+        "answer" : "they look down on someone."
+    }, 
+
+    10 : {
+        "question" : "Wenn Jemanden ist 'Uneingennuetzig', bedeuetet es, dass sie sind . . ." , 
+        "answer" : "selfless"
+    }, 
+
+    11 : {
+         "question" : "Wenn Jemanden ist 'ueberheblich', bedeuetet es, dass sie sind. . ." , 
+         "answer" : "overbearing"
+    }, 
+
+    12 : {
+         "question" : "'Entsprechen' bedeutet . . ." , 
+         "answer" : "to correspond to"
+    }, 
+
+    13 : {
+         "question" : "'Schweigen' bedeutet. . ." , 
+         "answer" : "nichts sagen"
+    }, 
+
+    14 : {
+         "question" : "'Prahlen' bedeutet. . ." , 
+         "answer" : "to boast"
+    }, 
+
+    15 : {
+         "question" : "'Einschuechtern' bedeutet. . ." , 
+         "answer" : "to intimidate (somebody)"
+    }, 
+
+    16 : {
+         "question" : "'Veraengstigen' bedeutet. . ." , 
+         "answer" : "to spook someone"
+    }, 
+
+    17 : {
+         "question" : "'Bedrohen' bedeutet. . ." , 
+         "answer" : "to threaten (somebody)"
+    }, 
+
+    18 : {
+         "question" : "'Besänftigen' bedeutet. . ." , 
+         "answer" : "to placate"
+    }, 
+
+    19 : {
+         "question" : "Ein 'Elfenbeinturm' ist. . ." , 
+         "answer" : "an ivory tower"
+    }, 
+
+    20 : {
+         "question" : "Eine Scheune ist. . ." , 
+         "answer" : "a barn"
+    }, 
+
+    21 : {
+         "question" : "Ein 'Ameisenhuegel' ist. . ." , 
+         "answer" : "an anthill"
+    }, 
+
+    22 : {
+         "question" : "Einer 'Bienenburg' ist. . ." , 
+         "answer" : "a beehive"
+    }, 
+
+    23 : {
+         "question" : "'Flanieren' bedeutet. . ." , 
+         "answer" : "to saunter or stroll"
+    }, 
+
+    24 : {
+         "question" : "'Taumeln' bedeutet. . ." , 
+         "answer" : "to stagger"
+    }, 
+
+    25 : {
+         "question" : "'Schlurfen' bedeutet. . ." , 
+         "answer" : "to shuffle"
+    }, 
+
+    26 : {
+         "question" : "'Hinken' bedeutet. . ." , 
+         "answer" : "to limp"
+    }, 
+
+    27 : {
+         "question" : "Ein 'Marschroute' ist. . ." , 
+         "answer" : "a strategy"
+    }, 
+
+    28 : {
+         "question" : "'Stapfen' bedeutet. . ." , 
+         "answer" : "to trudge"
+    }, 
+
+    29 : {
+         "question" : "Ein 'Sonderling' ist. . ." , 
+         "answer" : "a misfit"
+    }, 
+
+    30 : {
+         "question" : "Ein 'Eigenbrötler' ist. . ." , 
+         "answer" : "a loner"
+    }, 
+
+    31 : {
+         "question" : "Ein 'Taugenichts' ist. . ." , 
+         "answer" : "a slacker"
+    }, 
+
+    32 : {
+         "question" : "Ein 'Schwaechtling' ist. . ." , 
+         "answer" : "a pushover"
+    }, 
+
+    33 : {
+         "question" : "Ein 'Versager' ist. . ." , 
+         "answer" : "a deadbeat"
+    }, 
+
+    34 : {
+         "question" : "Ein 'Emporkoemmling' ist. . ." , 
+         "answer" : "a self-starter"
+    }, 
+
+    35 : {
+         "question" : "Ein 'Boesewicht' ist. . ." , 
+         "answer" : "a villain"
+    }, 
+
+    36 : {
+         "question" : "Ein 'Fiesling' ist. . ." , 
+         "answer" : "a schmuck"
+    }, 
+
+    37 : {
+         "question" : "Ein 'Widerling' ist. . ." , 
+         "answer" : "a sleazeball"
+    }, 
+
+    38 : {
+         "question" : "Ein 'Schurke' ist. . ." , 
+         "answer" : "a villain"
+    }, 
+
+    39 : {
+         "question" : "'Zuechten' bedeutet. . ." , 
+         "answer" : "to grow or breed something"
+    }, 
+
+    40 : {
+         "question" : "'Auftreten' bedeutet. . ." , 
+         "answer" : "to occur"
+    }, 
+
+    41 : {
+         "question" : "Ein 'Umstand' ist. . ." , 
+         "answer" : "a circumstance"
+    }, 
+    
+    42 : {
+         "question" : "'Stammen' bedeutet. . ." , 
+         "answer" : "to descend from" 
+    }, 
+
+    43 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    44 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    45 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    46 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    47 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    48 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    49 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+    
+    50 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    51 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    52 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    53 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    54 : {
+         "question" : "" , 
+         "answer" : "" 
+    }, 
+
+    55 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    56 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    57 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    58 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    59 : {
+        "question" : "" , 
+        "answer" : ""
+    }, 
+
+    60 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    61 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    62 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    63 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    64 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    65 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    66 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    67 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    68 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    69 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    70 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    71 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    72 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    73 : {
+         "question" : "" , 
+         "answer" : "" 
+    }, 
+
+    74 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    75 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    76 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    77: {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    78 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    79 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    80 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    81 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    82 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    83 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    84 : {
+         "question" : "" , 
+         "answer" : "" 
+    }, 
+
+    85 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    86 : {
+         "question" : "" , 
+         "answer" : "" 
+    }, 
+
+    87 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    88 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    89 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    90 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    91 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    92 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    93 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    94 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    95 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    96 : {
+         "question" : "" , 
+         "answer" : "" 
+    }, 
+
+    97 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    98 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    99 : {
+         "question" : "" , 
+         "answer" : ""
+    }, 
+
+    100 : {
+          "question" : "" , 
+          "answer" : ""
+    }, 
+
+}
